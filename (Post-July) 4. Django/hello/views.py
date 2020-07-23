593f818642260af82c3cf4bf94e855d522bff881
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2020-07-23 18:29:36 +0800

    initialise SQL folder

--- (Post-July) 4. Django/hello/views.py
+++ (Post-July) 4. Django/hello/views.py
@@ -5,6 +5,7 @@ def index0(request):
     return HttpResponse("Hello, world!")
 
 def index1(request):
+    # prefix with hello to differentiate greet.html from hello app with greet.html from other apps
     return render(request, "hello/index.html")
 
 def brian(request):
@@ -16,6 +17,7 @@ def david(request):
 def greet0(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}!")
 
+# Renders an entire html file instead of just a string
 def greet1(request, name):
     return render(request, "hello/greet.html", {
         "name": name.capitalize()
--- (Post-July) 5. SQL, Models, and Migrations/airline/manage.py
+++ (Post-July) 5. SQL, Models, and Migrations/airline/manage.py
@@ -3,10 +3,9 @@
 import os
 import sys
 
-# python manage.py runserver
-# python manage.py migrate
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lecture3.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airline.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
